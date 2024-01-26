e75b302a216a28a63a315df4c21035c366382aa6
Cody Hodges <chodges@appddictionstudio.com>
2024-01-26 15:24:44 -0600

    Add Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 
-injectSpeedInsights();
+inject();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
